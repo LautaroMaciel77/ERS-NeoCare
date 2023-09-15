b503e5a0c17e0b2208958e54bc6c76f000018446
Farid <farid.gpasi@gmail.com>
2023-09-14 23:49:28 -0300

    wip vista paciente

--- ERS-NeoCare/Design/paciente.Designer.cs
+++ ERS-NeoCare/Design/paciente.Designer.cs
@@ -28,18 +28,266 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.LHistoriaClinica = new System.Windows.Forms.Label();
+            this.PBPaciente = new System.Windows.Forms.PictureBox();
+            this.LNombre = new System.Windows.Forms.Label();
+            this.LApellido = new System.Windows.Forms.Label();
+            this.LDni = new System.Windows.Forms.Label();
+            this.LSexo = new System.Windows.Forms.Label();
+            this.LDomicilio = new System.Windows.Forms.Label();
+            this.LFechaNacimiento = new System.Windows.Forms.Label();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.LObraSocial = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.CEvento = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CDatos = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.CAcciones = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PBPaciente)).BeginInit();
+            this.panel5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
-            // UserControl1
+            // panel1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(53)))), ((int)(((byte)(76)))));
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.dataGridView1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 452);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1011, 247);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.White;
+            this.panel2.Controls.Add(this.panel5);
+            this.panel2.Controls.Add(this.panel4);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(296, 452);
+            this.panel2.TabIndex = 1;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(242)))), ((int)(((byte)(247)))));
+            this.panel3.Controls.Add(this.LHistoriaClinica);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(296, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(715, 452);
+            this.panel3.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(53)))), ((int)(((byte)(76)))));
+            this.panel4.Controls.Add(this.PBPaciente);
+            this.panel4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel4.Location = new System.Drawing.Point(0, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(296, 165);
+            this.panel4.TabIndex = 0;
+            // 
+            // LHistoriaClinica
+            // 
+            this.LHistoriaClinica.AutoSize = true;
+            this.LHistoriaClinica.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.LHistoriaClinica.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(53)))), ((int)(((byte)(76)))));
+            this.LHistoriaClinica.Location = new System.Drawing.Point(294, 23);
+            this.LHistoriaClinica.Name = "LHistoriaClinica";
+            this.LHistoriaClinica.Size = new System.Drawing.Size(135, 18);
+            this.LHistoriaClinica.TabIndex = 0;
+            this.LHistoriaClinica.Text = "Historia Clinica";
+            this.LHistoriaClinica.Click += new System.EventHandler(this.LHistoriaClinica_Click);
+            // 
+            // PBPaciente
+            // 
+            this.PBPaciente.BackgroundImage = global::ERS_NeoCare.Properties.Resources.user_background_icon;
+            this.PBPaciente.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.PBPaciente.InitialImage = null;
+            this.PBPaciente.Location = new System.Drawing.Point(88, 23);
+            this.PBPaciente.Name = "PBPaciente";
+            this.PBPaciente.Size = new System.Drawing.Size(120, 120);
+            this.PBPaciente.TabIndex = 0;
+            this.PBPaciente.TabStop = false;
+            // 
+            // LNombre
+            // 
+            this.LNombre.AutoSize = true;
+            this.LNombre.Location = new System.Drawing.Point(16, 24);
+            this.LNombre.Name = "LNombre";
+            this.LNombre.Size = new System.Drawing.Size(50, 13);
+            this.LNombre.TabIndex = 1;
+            this.LNombre.Text = "Nombre: ";
+            // 
+            // LApellido
+            // 
+            this.LApellido.AutoSize = true;
+            this.LApellido.Location = new System.Drawing.Point(16, 46);
+            this.LApellido.Name = "LApellido";
+            this.LApellido.Size = new System.Drawing.Size(47, 13);
+            this.LApellido.TabIndex = 2;
+            this.LApellido.Text = "Apellido:";
+            this.LApellido.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // LDni
+            // 
+            this.LDni.AutoSize = true;
+            this.LDni.Location = new System.Drawing.Point(16, 73);
+            this.LDni.Name = "LDni";
+            this.LDni.Size = new System.Drawing.Size(29, 13);
+            this.LDni.TabIndex = 3;
+            this.LDni.Text = "DNI:";
+            // 
+            // LSexo
+            // 
+            this.LSexo.AutoSize = true;
+            this.LSexo.Location = new System.Drawing.Point(16, 122);
+            this.LSexo.Name = "LSexo";
+            this.LSexo.Size = new System.Drawing.Size(34, 13);
+            this.LSexo.TabIndex = 4;
+            this.LSexo.Text = "Sexo:";
+            this.LSexo.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // LDomicilio
+            // 
+            this.LDomicilio.AutoSize = true;
+            this.LDomicilio.Location = new System.Drawing.Point(16, 150);
+            this.LDomicilio.Name = "LDomicilio";
+            this.LDomicilio.Size = new System.Drawing.Size(52, 13);
+            this.LDomicilio.TabIndex = 5;
+            this.LDomicilio.Text = "Domicilio:";
+            // 
+            // LFechaNacimiento
+            // 
+            this.LFechaNacimiento.AutoSize = true;
+            this.LFechaNacimiento.Location = new System.Drawing.Point(16, 99);
+            this.LFechaNacimiento.Name = "LFechaNacimiento";
+            this.LFechaNacimiento.Size = new System.Drawing.Size(109, 13);
+            this.LFechaNacimiento.TabIndex = 6;
+            this.LFechaNacimiento.Text = "Fecha de nacimiento:";
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.LObraSocial);
+            this.panel5.Controls.Add(this.LFechaNacimiento);
+            this.panel5.Controls.Add(this.LDomicilio);
+            this.panel5.Controls.Add(this.LNombre);
+            this.panel5.Controls.Add(this.LSexo);
+            this.panel5.Controls.Add(this.LApellido);
+            this.panel5.Controls.Add(this.LDni);
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel5.Location = new System.Drawing.Point(0, 165);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(296, 287);
+            this.panel5.TabIndex = 1;
+            // 
+            // LObraSocial
+            // 
+            this.LObraSocial.AutoSize = true;
+            this.LObraSocial.Location = new System.Drawing.Point(16, 174);
+            this.LObraSocial.Name = "LObraSocial";
+            this.LObraSocial.Size = new System.Drawing.Size(63, 13);
+            this.LObraSocial.TabIndex = 7;
+            this.LObraSocial.Text = "Obra social:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.CEvento,
+            this.CDatos,
+            this.CAcciones});
+            this.dataGridView1.Location = new System.Drawing.Point(44, 52);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(943, 171);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(462, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Historial";
+            // 
+            // CEvento
+            // 
+            this.CEvento.HeaderText = "Evento";
+            this.CEvento.Name = "CEvento";
+            this.CEvento.ReadOnly = true;
+            // 
+            // CDatos
+            // 
+            this.CDatos.HeaderText = "Fecha";
+            this.CDatos.Name = "CDatos";
+            this.CDatos.ReadOnly = true;
+            // 
+            // CAcciones
+            // 
+            this.CAcciones.HeaderText = "Acciones";
+            this.CAcciones.Name = "CAcciones";
+            this.CAcciones.ReadOnly = true;
+            // 
+            // paciente
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Name = "UserControl1";
-            this.Size = new System.Drawing.Size(508, 409);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "paciente";
+            this.Size = new System.Drawing.Size(1011, 699);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.PBPaciente)).EndInit();
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label LHistoriaClinica;
+        private System.Windows.Forms.PictureBox PBPaciente;
+        private System.Windows.Forms.Label LApellido;
+        private System.Windows.Forms.Label LNombre;
+        private System.Windows.Forms.Label LDni;
+        private System.Windows.Forms.Label LDomicilio;
+        private System.Windows.Forms.Label LSexo;
+        private System.Windows.Forms.Label LFechaNacimiento;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Label LObraSocial;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CEvento;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CDatos;
+        private System.Windows.Forms.DataGridViewTextBoxColumn CAcciones;
     }
 }
